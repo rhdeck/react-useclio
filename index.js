75d042374a9caa099b40aae5f440ce55026d9f61
Ray Deck <ray@raydeck.com>
2019-07-05 17:28:57 -0400

    Fixing uri/url

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const useClio = ({ location, clientId, redirectUri, state }) => {
   if (!redirectUri) {
     redirectUri = location ? location.href : window.location.href;
   }
-  const { code, uri, status, state: returnedState } = useOauth({
+  const { code, url, status, state: returnedState } = useOauth({
     authorizeUrl: "https://app.clio.com/oauth/approval",
     state,
     clientId,
     redirectUri
   });
-  return { uri, status, code, state: returnedState };
+  return { url, status, code, state: returnedState };
 };
 export default useClio;
